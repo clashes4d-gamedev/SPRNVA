e0bd18e3a4e33ee0f2565e3709ff1571e07f44f4
clashes4d-gamedev <97254636+clashes4d-gamedev@users.noreply.github.com>
2022-03-29 01:05:10 +0200

    Added the Ability to switch Boundaries on and off

--- entity_controller.py
+++ entity_controller.py
@@ -2,14 +2,13 @@ import pygame
 from .vector import Vector
 
 class Player:
-    def __init__(self, win, pos: Vector, size: Vector, vel, img='', color=(255, 0, 0)):
+    def __init__(self, win, pos: Vector, size: Vector, img='', color=(255, 0, 0)):
         self.win = win
         self.x = pos.x
         self.y = pos.y
         self.width = size.x
         self.height = size.y
         self.color = color
-        self.vel = vel
         self.collider = pygame.Rect(self.x, self.y, self.width, self.height)
         self.surf = pygame.Surface((self.collider.width, self.collider.height))
         self.img = pygame.image.load(img).convert_alpha() if img != '' else img
@@ -28,33 +27,61 @@ class Player:
                 collisions.append(tile)
         return collisions
 
-    def move(self, tiles, x=False, y=False, minus_y=False, minus_x=False):
-        if self.vel >= 0:
-            if x is not False and (self.collider.x + self.collider.width) <= self.win.get_width():
-                self.collider.x += self.vel
-                collisions = self.check_collisions(tiles)
-                for tile in collisions:
-                    self.collider.right = tile.left
+    def move(self, tiles, vel, x=False, y=False, minus_y=False, minus_x=False, boundaries=True):
+        if vel >= 0:
+            if boundaries:
+                if x is not False and (self.collider.x + self.collider.width) <= self.win.get_width():
+                    self.collider.x += vel.x
+                    collisions = self.check_collisions(tiles)
+                    for tile in collisions:
+                        self.collider.right = tile.left
 
-            elif y is not False and (self.collider.y + self.collider.height) <= self.win.get_height():
-                self.collider.y += self.vel
-                collisions = self.check_collisions(tiles)
-                for tile in collisions:
-                    self.collider.bottom = tile.top
+                elif y is not False and (self.collider.y + self.collider.height) <= self.win.get_height():
+                    self.collider.y += vel.y
+                    collisions = self.check_collisions(tiles)
+                    for tile in collisions:
+                        self.collider.bottom = tile.top
 
-            elif minus_x is not False and self.collider.x >= 0:
-                self.collider.x -= self.vel
-                collisions = self.check_collisions(tiles)
-                for tile in collisions:
-                    self.collider.left = tile.right
+                elif minus_x is not False and self.collider.x >= 0:
+                    self.collider.x -= vel.x
+                    collisions = self.check_collisions(tiles)
+                    for tile in collisions:
+                        self.collider.left = tile.right
 
-            elif minus_y is not False and self.collider.y >= 0:
-                self.collider.y -= self.vel
-                collisions = self.check_collisions(tiles)
-                for tile in collisions:
-                    self.collider.top = tile.bottom
+                elif minus_y is not False and self.collider.y >= 0:
+                    self.collider.y -= vel.y
+                    collisions = self.check_collisions(tiles)
+                    for tile in collisions:
+                        self.collider.top = tile.bottom
 
+                else:
+                    return
             else:
-                return
+                if x is not False:
+                    self.collider.x += vel.x
+                    collisions = self.check_collisions(tiles)
+                    for tile in collisions:
+                        self.collider.right = tile.left
+
+                elif y is not False:
+                    self.collider.y += vel.y
+                    collisions = self.check_collisions(tiles)
+                    for tile in collisions:
+                        self.collider.bottom = tile.top
+
+                elif minus_x is not False:
+                    self.collider.x -= vel.x
+                    collisions = self.check_collisions(tiles)
+                    for tile in collisions:
+                        self.collider.left = tile.right
+
+                elif minus_y is not False:
+                    self.collider.y -= vel.y
+                    collisions = self.check_collisions(tiles)
+                    for tile in collisions:
+                        self.collider.top = tile.bottom
+
+                else:
+                    return
         else:
             return
